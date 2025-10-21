16e3cbc0425dac9e55bf77ea27acc7964b358a73
Lucas Barbosa <29305833+lucasbarbosaz@users.noreply.github.com>
2025-10-20 22:23:48 -0300

    criando model manualmente e com o cli

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Meu primeiro projeto com NestJS!';
   }
+  
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
+/*
+src/app.module.ts: Modulo principal do aplicativo
+src/app.controller.ts: Define as rotas e lida com requisições.
+src/app.service.ts: Contém a lógica de negocio, separado do controller.
+*/
+
+// Arquivo que inicia nosso projeto NestJS
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
