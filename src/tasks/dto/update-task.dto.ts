51c650d394154a7608ce840a2110246b74f0e3c0
Lucas Barbosa <29305833+lucasbarbosaz@users.noreply.github.com>
2025-10-21 00:33:42 -0300

    reutilizando dto com partial type

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,19 +1,14 @@
-import { IsBoolean, IsOptional, IsString } from "class-validator";
+import { PartialType } from "@nestjs/mapped-types";
+import { IsBoolean, IsOptional } from "class-validator";
+import { CreateTaskDto } from "./create-task.dto";
 
 /**
  * DTO -> Data Transfer Object (Objeto de Transferência de Dados)
  * -> Validar dados, transformar dados
  * -> Se usa para representar quais dados e em que formatos uma determinada camada aceita e trabalha
  */
-export class UpdateTaskDto {
-  @IsString({ message: "O nome da tarefa deve ser uma string!" })
-  @IsOptional()
-  readonly name?: string;
 
-  @IsString({ message: "O nome da tarefa deve ser uma string!" })
-  @IsOptional()
-  readonly description?: string;
-
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
   @IsBoolean({ message: "O campo completed deve ser um boolean!" })
   @IsOptional()
   readonly completed?: boolean;
