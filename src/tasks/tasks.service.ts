23078be582c4040094c0b33a2d780f1bd3a8cd46
Lucas Barbosa <29305833+lucasbarbosaz@users.noreply.github.com>
2025-10-20 23:26:39 -0300

    listando itens e salvando

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,18 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
+
+  private tasks: Task[] = [
+    {
+      id: 1,
+      name: "Estudar NestJS",
+      description: "Estudar o framework NestJS para construir aplicações backend escaláveis.",
+      completed: false
+    }
+  ]
+
   findAll() {
-    return [ 
-      { id: 1, task: "Estudar NestJS" }
-    ]
+    return this.tasks;
   }
 
   findOne(id: string) {
-    return "Buscando a tarefa de id " + id;
+    return this.tasks.find( task => task.id === Number(id) ) // verificando se o id da task é igual ao id passado como parâmetro
   }
 
   create(body: any) {
-    return body;
+    const newId = this.tasks.length + 1; // gerando um novo id baseado no tamanho do array
+    const newTask: Task = {
+      id: newId,
+      ...body
+    }
+
+    this.tasks.push(newTask);
+
+    return newTask;
   }
 }
